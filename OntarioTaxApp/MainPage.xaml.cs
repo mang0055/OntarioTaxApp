1f831340231b5cc2d02db1a10695748345d64436
ravirajcm <mang0055@algonquinlive.com>
2017-03-06 15:57:49 -0500

    Done with assignment

--- OntarioTaxApp/MainPage.xaml.cs
+++ OntarioTaxApp/MainPage.xaml.cs
@@ -19,24 +19,39 @@ using Windows.UI.Xaml.Navigation;
 namespace OntarioTaxApp
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// An page is Main Page of application. Which have all controls to calculate and display Tax.
     /// </summary>
     public sealed partial class MainPage : Page
     {
         Tax tax;
+
         string taxAmountInputValue = string.Empty;
         string taxInputValue = string.Empty;
+
+        /// <summary>
+        /// MainPage initialization
+        /// </summary>
         public MainPage()
         {
             this.InitializeComponent();
             tax = new Tax();
         }
 
+        /// <summary>
+        /// WHile user write text inside purchase amount input field this method will call. where we are calculating tax based on input so user can see calculated tax realtime.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void purchaseAmount_TextChanged(object sender, TextChangedEventArgs e)
         {
             doTaxCalculation(purchaseAmount.Text);
         }
 
+        /// <summary>
+        /// Purchase amount input Got Focus. So while user opens app its tab index 0. means active. Ready for accept inputs.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void purchaseAmount_GotFocus(object sender, RoutedEventArgs e)
         {
             purchaseAmount.Text = string.Empty;
@@ -44,29 +59,48 @@ namespace OntarioTaxApp
             totalAmount.Text = "$0.00";
         }
 
+        /// <summary>
+        /// Purchase Amount input Lost Focus event. So while user enter text and click outside or click on other element of screen. it will execute this method where we are calculating tax and setting purchase formated price.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void purchaseAmount_LostFocus(object sender, RoutedEventArgs e)
         {
-           
             doTaxCalculation(purchaseAmount.Text);
             purchaseAmount.Text = tax.purchasePrice;
-
         }
+
+        /// <summary>
+        /// All RadioButton clicks call back.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void GSTRadioButton_Click(object sender, RoutedEventArgs e)
         {
             doTaxCalculation(purchaseAmount.Text);
             purchaseAmount.Text = tax.purchasePrice;
         }
 
+        /// <summary>
+        /// Method: doTaxCalculations will accept input value from user and find Appropriate selected Tax.
+        /// </summary>
+        /// <param name="inputvalue"></param>
         private void doTaxCalculation(string inputvalue)
         {
+            //This method working as below steps
+            //1. This forEach loop though all main stack panel Radio button and find checked one. It will extract string of Tag from Radiobutton which is actual Tax value.
             foreach (RadioButton tb in FindVisualChildren<RadioButton>(mainStackPanel))
             {
                 if (tb.IsChecked == true)
                 {
+                    //2. Tax value and Purchase value are passing to Tax class reference in Calculate method.
                     tax.Calculate(inputvalue, tb.Tag.ToString());
-                    if(tax.totalTax!=string.Empty)
+
+                    //3. Setting calculated Total Tax value from tax class's method into appropriate Label if its not Empty
+                    if (tax.totalTax!=string.Empty)
                     taxAmount.Text = tax.totalTax;
-                    if(tax.totalPrice!=string.Empty)
+                    //4. Setting calculated Total Price value from tax class's method into appropriate Label if its not Empty
+                    if (tax.totalPrice!=string.Empty)
                     totalAmount.Text = tax.totalPrice;
                 }
             }
--- OntarioTaxApp/Tax.cs
+++ OntarioTaxApp/Tax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Raviraj Mangukiya
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,28 +13,49 @@ namespace OntarioTaxApp
         public string purchasePrice;
         public string totalTax;
         public string totalPrice;
-
+        /// <summary>
+        /// Constructor 
+        /// </summary>
         public Tax()
         {
             purchasePrice = string.Empty;
             totalTax = string.Empty;
             totalPrice = string.Empty;
         }
+        /// <summary>
+        /// Method: Calculate will Do calculation based on Input Tax and Choosen Tax. And Set values according to its container.
+        /// </summary>
+        /// <param name="purchasePrice"></param>
+        /// <param name="taxPrice"></param>
         public void Calculate(string purchasePrice, string taxPrice) {
             double pPrice = 0.0;
             double tPrice = 0.0;
-            double.TryParse(purchasePrice.Replace('$',' '), out pPrice);
-            double.TryParse(taxPrice, out tPrice);
 
-            double totalTax1 = pPrice * (tPrice / 100);
-            double priceWithTotalTax = pPrice + totalTax1;
+            //Converting Strings to Purchased Price into Double.
+            bool bPrice =double.TryParse(purchasePrice.Replace('$',' '), out pPrice);
+            if (bPrice)
+            {
+                //Converting Strings to Tax Price into Double.
+                bool bTax = double.TryParse(taxPrice, out tPrice);
+                if (bTax)
+                {
+                    double totalTax1 = pPrice * (tPrice / 100);
+                    double priceWithTotalTax = pPrice + totalTax1;
 
+                    //Formating Back to Currency formate from Double.
+                    this.purchasePrice = string.Format("{0:C}", pPrice);
+                    this.totalTax = string.Format("{0:C}", totalTax1);
+                    this.totalPrice = string.Format("{0:C}", priceWithTotalTax);
+                }
+                //Handling Error to print for now
+                else {
+                    Debug.WriteLine("Error Parsing Tax: " + taxPrice);
+                }
+            }
+            else {
+                Debug.WriteLine("Error Parsing Purchase Price: " + purchasePrice);
+            }
            
-            //this.purchasePrice = purchasePrice;
-            this.purchasePrice = string.Format("{0:C}", pPrice);
-            Debug.WriteLine(this.purchasePrice);
-            this.totalTax = string.Format("{0:C}", totalTax1);
-            this.totalPrice = string.Format("{0:C}",priceWithTotalTax);
         }
     }
 }
